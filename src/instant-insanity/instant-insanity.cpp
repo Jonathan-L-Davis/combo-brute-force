b1c76f3e30db1f560432bc60881c714d16a4af9d
Jon Davis <jldavisjr@yahoo.com>
2025-02-21 13:25:56 -0500

    3 down. Stopped to play with the dogs for a bit.

--- src/instant-insanity/instant-insanity.cpp
+++ src/instant-insanity/instant-insanity.cpp
@@ -1,5 +1,29 @@
 #include <iostream>
+#include <algorithm>
 #include <cstdint>
+#include <set>
+
+// stoler from SO, ty very mooch astraujums, https://stackoverflow.com/questions/283977/c-stl-set-difference
+template<typename T>
+std::set<T> operator -(std::set<T> reference, std::set<T> items_to_remove)
+{
+    std::set<T> result;
+    std::set_difference(
+        reference.begin(), reference.end(),
+        items_to_remove.begin(), items_to_remove.end(),
+        std::inserter(result, result.end()));
+    return result;
+}
+// end SO attributed code.
+
+template<typename T>
+std::set<T> operator -=(std::set<T>& result, std::set<T> subMe){
+    result = result - subMe;
+    return result;
+}
+
+// the two coob types are useful for the population vs the rotation parts. Rotation only blocks out one side of the 
+
 
 template<uint32_t D>
 struct cube_opposite_faces{
@@ -7,17 +31,16 @@ struct cube_opposite_faces{
     uint8_t f2[D];
 };
 
-static_assert(sizeof(cube_opposite_faces<3> == 6));
-static_assert(sizeof(cube_opposite_faces<4> == 8));
+static_assert(sizeof(cube_opposite_faces<3>) == 6);
+static_assert(sizeof(cube_opposite_faces<4>) == 8);
 
 template<uint32_t D>
 struct cube{
-    uint8_t f1[D];
-    uint8_t f2[D];
+    uint8_t faces[2*D];
 };
 
-static_assert(sizeof(cube<3> == 6));
-static_assert(sizeof(cube<4> == 8));
+static_assert(sizeof(cube<3>) == 6);
+static_assert(sizeof(cube<4>) == 8);
 
 template<uint32_t D>
 struct chain{
@@ -25,13 +48,14 @@ struct chain{
 };
 
 template<uint32_t D>
-cube<D> next_color(uint64_t index){
+cube<D> get_cube(uint64_t index){
     cube<D> retMe;
+    retMe.faces[0] = 0;
     
-    index++;
+    std::set faces_left = {1,2,3,4,5};
     
-    retMe.f1
     
+    index /= 5;
     
     return retMe;
 }
