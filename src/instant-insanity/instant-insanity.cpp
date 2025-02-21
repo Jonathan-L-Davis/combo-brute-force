770d594f9870ea7fe9fe43217961a644c980723a
Jon Davis <jldavisjr@yahoo.com>
2025-02-21 12:41:22 -0500

    2 down. ja mon

--- src/instant-insanity/instant-insanity.cpp
+++ src/instant-insanity/instant-insanity.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <cstdint>
 
-enum class f:uint8_t{red = 0, yellow = 0, green = 2, blue = 3};
+template<uint32_t D>
+struct cube_opposite_faces{
+    uint8_t f1[D];
+    uint8_t f2[D];
+};
 
+static_assert(sizeof(cube_opposite_faces<3> == 6));
+static_assert(sizeof(cube_opposite_faces<4> == 8));
 
 template<uint32_t D>
 struct cube{
@@ -10,10 +16,30 @@ struct cube{
     uint8_t f2[D];
 };
 
+static_assert(sizeof(cube<3> == 6));
+static_assert(sizeof(cube<4> == 8));
+
+template<uint32_t D>
+struct chain{
+    cube<D> arr[4];
+};
+
+template<uint32_t D>
+cube<D> next_color(uint64_t index){
+    cube<D> retMe;
+    
+    index++;
+    
+    retMe.f1
+    
+    
+    return retMe;
+}
+
 int main(){
     
     std::cout << "Instant insanity brute force." << "\n";
-    
+    int total_solutions = 0;
     
     
     
